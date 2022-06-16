7c72c1e4e6721f045a9e012a41fe4e1fd8553026
WDamaris <damarisshish@gmail.com>
2022-06-16 15:45:29 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+let oldText=document.getElementById("text")
+document.addEventListener("DOMContentLoaded",addText)
+    function addText() {
+       return text.innerHTML="This is really cool!"
+    }    
+
+
+
